a30b959d557d9617a0ba1016dee8a2bd7b21979d
Lilo <rick@ouioui-online.fr>
2022-04-05 18:58:41 +0200

    Added and made the 'experience' part of the 'about page' work correctly, according to the json file.

--- script.js
+++ script.js
@@ -13,19 +13,19 @@ function go_to_page(obj){
 
 
 
-function removeeffect(txt){
-    document.getElementById("project_description").innerHTML = document.getElementById("project_description").innerHTML.replaceAll("</b>","").replaceAll("<b>","");
-    let InputText = document.getElementById("project_description").innerHTML
+function removeeffect(txt, text_to_change){
+    document.getElementById(text_to_change).innerHTML = document.getElementById(text_to_change).innerHTML.replaceAll("</b>","").replaceAll("<b>","");
+    let InputText = document.getElementById(text_to_change).innerHTML
     let maxrotation = InputText.length;
-    console.log(maxrotation, document.getElementById("project_description").innerHTML.length)
+    console.log(maxrotation, document.getElementById(text_to_change).innerHTML.length)
     let speed = 500/maxrotation;
     for(let i = 0; i<maxrotation; i++){ 
         speed += 500/maxrotation
         setTimeout(() => {
-            InputText = document.getElementById("project_description").innerHTML;
-            document.getElementById("project_description").innerHTML = InputText.slice(0, -1);
+            InputText = document.getElementById(text_to_change).innerHTML;
+            document.getElementById(text_to_change).innerHTML = InputText.slice(0, -1);
             if(i ==maxrotation-1){
-                typingeffect(txt);
+                typingeffect(txt, text_to_change);
             }
         }, speed);
         
@@ -33,14 +33,14 @@ function removeeffect(txt){
 }
 
 
-function typingeffect(txt){ 
+function typingeffect(txt, text_to_change){ 
     let bold_mode = false
     let speed = 500/txt.length 
     for (let i =0; i < txt.length; i++){
         speed += 500/txt.length;
         setTimeout(function(){
             if (txt.charAt(i) == "<"){ 
-                document.getElementById("project_description").innerHTML+= "<br>"}
+                document.getElementById(text_to_change).innerHTML+= "<br>"}
             else if (txt.charAt(i) == "+"){
                 bold_mode = true;
                 console.log("true!" + charAt(i))
@@ -49,10 +49,10 @@ function typingeffect(txt){
                 bold_mode = false
             }
             else if (bold_mode){
-                document.getElementById("project_description").innerHTML += txt.charAt(i).bold()
+                document.getElementById(text_to_change).innerHTML += txt.charAt(i).bold()
             }
             else{
-                document.getElementById("project_description").innerHTML += txt.charAt(i);
+                document.getElementById(text_to_change).innerHTML += txt.charAt(i);
                 }
             }, speed);
 }}
--- vue.js
+++ vue.js
@@ -6,14 +6,14 @@ new Vue({
         skills: [],
         softskills: [],
         projects : [],
+        experiences: [],
         current_project : 0,
+        current_experience : 0,
         isSoftSkills : false,
         InExecution : false
     },
     mounted(){
-        this.getSkillsDataList()
-        this.getProjectsDataList()
-        this.getsoftSkillsDataList()
+        this.getAllData()
     },
     methods: {
         change_page(page){
@@ -21,67 +21,103 @@ new Vue({
         },
         toggle_skills(arg){
             this.isSoftSkills = arg
-        }
-        ,
+        },
+        getAllData(){ // GET ALL DATA FROM THE JSON FILES
+            fetch("skills.json").then(response => response.json()).then(data => (this.skills = data));
 
-        getSkillsDataList(){
-            fetch("skills.json")
-            .then(response => response.json())
-            .then(data => (this.skills = data));
+            fetch("softskills.json").then(response => response.json()).then(data => (this.softskills = data));
+
+            fetch("projects.json").then(response => response.json()).then(data => (this.projects = data));
+
+            fetch("experiences.json").then(response => response.json()).then(data => (this.experiences = data));
+
         },
-        getsoftSkillsDataList(){
-            fetch("softskills.json")
-            .then(response => response.json())
-            .then(data => (this.softskills = data));
-        },
-        getProjectsDataList(){
-            fetch("projects.json")
-            .then(response => response.json())
-            .then(data => (this.projects = data));
-        },
-        updateCurrentProject(input){
+
+
+        updateCurrentIndex(input){
+
+
+            if(this.page == "projects"){
+              
+                if(input == "-" && this.current_project!=0){
+                    this.current_project--
+                }else
+                if(input == "-" && this.current_project == 0){
+                    this.current_project = this.projects.length-1
+                }else
+                if(input == "+" && this.current_project != this.projects.length-1){
+                    this.current_project++
+                }else
+                if(input == "+" && this.current_project == this.projects.length-1){
+                    this.current_project = 0
+                }
+                document.getElementById("projectSelector").value = this.current_project + 1
+
+                removeeffect(this.projects[this.current_project].description, "project_description")
+            }else{
+
+               
+                if(input == "-" && this.current_experience!=0){
+                    this.current_experience--
+                }else
+                if(input == "-" && this.current_experience == 0){
+                    this.current_experience = this.experiences.length-1
+                }else
+                if(input == "+" && this.current_experience != this.experiences.length-1){
+                    this.current_experience++
+                }else
+                if(input == "+" && this.current_experience == this.experiences.length-1){
+                    this.current_experience = 0
+                }
+                document.getElementById("experienceSelector").value = this.current_experience + 1;
+
+                removeeffect(this.experiences[this.current_experience].description, "experience_description");
+                
+            }
+
+
             if(this.InExecution){
                 return
             }
-            this.InExecution = true;
-            document.getElementById("projectSelector").style.pointerEvents = "none";
-            document.getElementById("projectSelector").style.opacity = 0.1;
 
-            if(input == "-" && this.current_project!=0){
-                this.current_project--
-            }else
-            if(input == "-" && this.current_project == 0){
-                this.current_project = this.projects.length-1
-            }else
-            if(input == "+" && this.current_project != this.projects.length-1){
-                this.current_project++
-            }else
-            if(input == "+" && this.current_project == this.projects.length-1){
-                this.current_project = 0
-            }
-            document.getElementById("projectSelector").value = this.current_project + 1
-            removeeffect(this.projects[this.current_project].description)
+            this.InExecution = true;
+
+            document.getElementByClassName("IndexSelector")[0].style.pointerEvents = "none";
+            document.getElementByClassName("IndexSelector")[0].style.opacity = 0.1;
             setTimeout(() => {
-                document.getElementById("projectSelector").style.pointerEvents = "all"
-                document.getElementById("projectSelector").style.opacity = 1;
+                document.getElementByClassName("IndexSelector")[0].style.pointerEvents = "all"
+                document.getElementByClassName("IndexSelector")[0].style.opacity = 1;
                 this.InExecution = false
             }, 1000);
         
         },
-        selectOtherProject(){
+        
+        selectOtherIndex(){
+
+            if(this.page == "projects"){
+
+                this.current_project = document.getElementById("projectSelector").value-1;
+                removeeffect(this.projects[this.current_project].description, "project_description" );
+                
+            }else{
+
+                this.current_experience = document.getElementById("experienceSelector").value-1;
+                removeeffect(this.experiences[this.current_experience].description, "experience_description");
+            }
+            
             if(this.InExecution){
                 return
             }
-            document.getElementById("projectSelector").style.pointerEvents = "none";
-            document.getElementById("projectSelector").style.opacity = 0.1;
-            this.current_project = document.getElementById("projectSelector").value-1;
-            this.InExecution = true;
-            removeeffect(this.projects[this.current_project].description);
+            document.getElementByClassName("IndexSelector")[0].style.pointerEvents = "none";
+            document.getElementByClassName("IndexSelector")[0].style.opacity = 0.1;
             setTimeout(() => {
                 this.InExecution = false
-                document.getElementById("projectSelector").style.pointerEvents = "all"
-                document.getElementById("projectSelector").style.opacity = 1;
+                document.getElementByClassName("IndexSelector")[0].style.pointerEvents = "all"
+                document.getElementByClassName("IndexSelector")[0].style.opacity = 1;
             }, 1000);
+            
+            this.InExecution = true;
+            
         }
     },
 })
